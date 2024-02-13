bb57d7b15c19efba286cfb61f006e2e7185f6659
asuc <sucaurel@gmail.com>
2024-02-13 16:27:49 +0100

    juste Path

--- srcs/get_cmd_prompt.c
+++ srcs/get_cmd_prompt.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_cmd_prompt.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mbuchs <mbuchs@student.42.fr>              +#+  +:+       +#+        */
+/*   By: asuc <asuc@student.42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/04 20:18:51 by asuc              #+#    #+#             */
-/*   Updated: 2024/02/10 17:42:08 by mbuchs           ###   ########.fr       */
+/*   Updated: 2024/02/13 16:27:19 by asuc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,22 +43,13 @@ static char	*get_cwd(t_env *env)
 	return (tmp);
 }
 
-static int	set_prompt_top(t_data *data, char *hostname, t_env *env)
+static int	set_prompt_top(t_data *data, t_env *env)
 {
 	char	*tmp;
 
-	data->cmd_prompt = ft_strjoin_free(data->cmd_prompt, "@");
+	data->cmd_prompt = ft_strdup("\001\033[1;32m\002");
 	if (data->cmd_prompt == NULL)
-		return (error_free("ft_strjoin_free failed"));
-	data->cmd_prompt = ft_strjoin_free(data->cmd_prompt, hostname);
-	if (data->cmd_prompt == NULL)
-		return (error_free("ft_strjoin_free failed"));
-	data->cmd_prompt = ft_strjoin_free(data->cmd_prompt, "\001\033[0m\002:");
-	if (data->cmd_prompt == NULL)
-		return (error_free("ft_strjoin_free failed"));
-	data->cmd_prompt = ft_strjoin_free(data->cmd_prompt, "\001\033[1;34m\002");
-	if (data->cmd_prompt == NULL)
-		return (error_free("ft_strjoin_free failed"));
+		return (error_free("ft_strdup failed"));
 	tmp = get_cwd(env);
 	data->cmd_prompt = ft_strjoin_free(data->cmd_prompt, tmp);
 	free(tmp);
@@ -72,27 +63,7 @@ static int	set_prompt_top(t_data *data, char *hostname, t_env *env)
 
 int	get_cmd_prompt(t_data *data, t_env *env)
 {
-	int		fd;
-	ssize_t	bytes_read;
-	char	hostname[1024];
-
-	data->cmd_prompt = ft_strjoin_free(ft_strdup("\001\033[1;32m\002"),
-			get_env_value_string(env, "USER"));
-	if (data->cmd_prompt == NULL)
-		data->cmd_prompt = ft_strdup("minishell");
-	fd = open("/etc/hostname", O_RDONLY);
-	if (fd == -1)
-		return (error_free("open /etc/hostname"));
-	bytes_read = read(fd, hostname, 1024 - 1);
-	if (bytes_read == -1)
-	{
-		perror("read /etc/hostname");
-		close(fd);
+	if (set_prompt_top(data, env) == EXIT_FAILURE)
 		return (EXIT_FAILURE);
-	}
-	hostname[bytes_read - 1] = '\0';
-	if (set_prompt_top(data, hostname, env) == EXIT_FAILURE)
-		return (EXIT_FAILURE);
-	close(fd);
 	return (EXIT_SUCCESS);
 }
