c529de939d2e4a70bc6f1cae1f1e62c6fdd29261
asuc <sucaurel@gmail.com>
2024-03-23 22:12:14 +0100

    Mise à jour de la fonction execute_command et choose_case dans minishell.c

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: asuc <asuc@student.42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 17:08:03 by asuc              #+#    #+#             */
-/*   Updated: 2024/03/23 14:17:45 by asuc             ###   ########.fr       */
+/*   Updated: 2024/03/23 15:48:19 by asuc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,32 +38,86 @@ static int	ft_env(t_env *env)
 	return (0);
 }
 
+void	execute_command(t_command *command, t_env *env, t_data *data,
+		int input_fd, int output_fd)
+{
+	pid_t	pid;
+
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		return ;
+	}
+	if (pid == 0)
+	{
+		if (input_fd != STDIN_FILENO)
+		{
+			dup2(input_fd, STDIN_FILENO);
+			close(input_fd);
+		}
+		if (output_fd != STDOUT_FILENO)
+		{
+			dup2(output_fd, STDOUT_FILENO);
+			close(output_fd);
+		}
+		printf("command->cmd: %s\n", command->cmd);
+		for (int i = 0; command->args[i]; i++)
+			printf("command->args == %s\n", command->args[i]);
+		if (ft_strcmp(command->cmd, "exit") == 0)
+			ft_exit(data, env, "exit", 0);
+		else if (ft_strcmp(command->cmd, "cd") == 0)
+			ft_cd(data, env);
+		else if (ft_strcmp(command->cmd, "export") == 0)
+			ft_export(env, command->args[1]);
+		else if (ft_strcmp(command->cmd, "env") == 0)
+			ft_env(env);
+		else if (ft_strcmp(command->cmd, "unset") == 0)
+			ft_unset(env, data);
+		else if (ft_strcmp(command->cmd, "echo") == 0)
+			ft_echo(data);
+		else if (ft_strcmp(command->cmd, "pwd") == 0)
+			ft_pwd(env);
+		else
+			g_return_code = execve_path_env(command->cmd, command->args, env,
+					data);
+		exit(g_return_code);
+	}
+	else
+	{
+		waitpid(pid, &g_return_code, 0);
+	}
+}
+
 void	choose_case(t_env *env, t_data *data)
 {
 	t_command	*command;
+	int			pipe_fd[2];
+	int			prev_fd;
 
 	command = data->command_top;
+	prev_fd = STDIN_FILENO;
 	while (command)
 	{
-		if (data->command_top == NULL || data->command_top->cmd == NULL)
-			return ;
-		if (ft_strcmp(data->command_top->cmd, "exit") == 0)
+		if (ft_strcmp(command->cmd, "exit") == 0)
 			ft_exit(data, env, "exit", 0);
-		if (ft_strcmp(data->command_top->cmd, "cd") == 0)
-			ft_cd(data, env);
-		else if (ft_strcmp(data->command_top->cmd, "export") == 0)
-			ft_export(env, data->command_top->args[1]);
-		else if (ft_strcmp(data->command_top->cmd, "env") == 0)
-			ft_env(env);
-		else if (ft_strcmp(data->command_top->cmd, "unset") == 0)
-			ft_unset(env, data);
-		else if (ft_strcmp(data->command_top->cmd, "echo") == 0)
-			ft_echo(data);
-		else if (ft_strcmp(data->command_top->cmd, "pwd") == 0)
-			ft_pwd(env);
-		else
-			g_return_code = execve_path_env(data->command_top->cmd,
-					data->command_top->args, env, data);
+		if (command->next)
+		{
+			if (pipe(pipe_fd) == -1)
+			{
+				perror("pipe");
+				return ;
+			}
+		}
+		execute_command(command, env, data, prev_fd, pipe_fd[1]);
+		if (prev_fd != STDIN_FILENO)
+			close(prev_fd);
+		if (command->next)
+		{
+			close(pipe_fd[1]);
+			prev_fd = pipe_fd[0];
+		}
+		command = command->next;
 	}
 }
 
