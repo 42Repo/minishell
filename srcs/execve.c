7d942e576ad5bc283fd5878f8ab3ba3c5aff0a4c
asuc <sucaurel@gmail.com>
2024-03-23 14:27:42 +0100

    Mise à jour du fichier .gitignore et des fichiers de configuration de VS Code. Ajout de nouvelles variables dans la structure de commande. Mise à jour de la fonction execve.

--- srcs/execve.c
+++ srcs/execve.c
@@ -3,14 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   execve.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mbuchs <mbuchs@student.42.fr>              +#+  +:+       +#+        */
+/*   By: asuc <asuc@student.42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/24 00:16:17 by asuc              #+#    #+#             */
-/*   Updated: 2024/03/16 19:50:04 by mbuchs           ###   ########.fr       */
+/*   Updated: 2024/03/23 14:20:14 by asuc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
 extern int	g_return_code;
 
 int	execve_error(char *path)
@@ -155,8 +156,12 @@ int	execve_path_env(char *cmd, char **args, t_env *env, t_data *data)
 	}
 	if (pid == 0)
 	{
-		(void)args;
-		// dup2(data->fd_out, 1);
+		if (check_exec_command(path))
+		{
+			free_tab(envp);
+			free(path);
+			ft_exit(data, env, "", 0);
+		}
 		i = execve(path, args, envp);
 		perror("minishell");
 		free_tab(envp);
@@ -168,3 +173,112 @@ int	execve_path_env(char *cmd, char **args, t_env *env, t_data *data)
 	free(path);
 	return (i);
 }
+
+// char **env_to_array(t_env *env)
+// {
+// 	t_env	*tmp;
+// 	int		i;
+// 	char	**envp;
+
+// 	tmp = env;
+// 	i = 0;
+// 	while (tmp)
+// 	{
+// 		i++;
+// 		tmp = tmp->next;
+// 	}
+// 	envp = malloc(sizeof(char *) * (i + 1));
+// 	if (envp == NULL)
+// 		return (NULL);
+// 	tmp = env;
+// 	i = 0;
+// 	while (tmp)
+// 	{
+// 		envp[i] = ft_strjoin(tmp->name, "=");
+// 		envp[i] = ft_strjoin_free(envp[i], tmp->value);
+// 		tmp = tmp->next;
+// 		i++;
+// 	}
+// 	envp[i] = NULL;
+// 	return (envp);
+// }
+
+// void execute_command(t_command *command, t_env *env)
+// {
+// 	// Créer un tableau pour stocker les arguments de la commande
+// 	int args_count = ft_tablen(command->args);
+// 	char **args = malloc(sizeof(char *) * (args_count + 2));
+// 	args[0] = command->cmd;
+// 	for (int i = 0; i < args_count; i++)
+// 	{
+// 		args[i + 1] = command->args[i];
+// 	}
+// 	args[args_count + 1] = NULL;
+
+// 	// Convertir la liste chaînée d'environnement en tableau de chaînes
+// 	char **envp = env_to_array(env);
+
+// 	// Exécuter la commande dans un nouveau processus
+// 	pid_t pid = fork();
+// 	if (pid == 0)
+// 	{
+// 		// Processus fils
+// 		if (execve(command->cmd, args, envp) == -1)
+// 		{
+// 			perror("execve");
+// 			exit(EXIT_FAILURE);
+// 		}
+// 	}
+// 	else if (pid < 0)
+// 	{
+// 		// Erreur lors de la création du processus fils
+// 		perror("fork");
+// 	}
+// 	else
+// 	{
+// 		// Processus parent
+// 		wait(NULL);
+// 	}
+
+// 	// Libérer la mémoire allouée
+// 	free(args);
+// 	free_array(envp);
+// }
+
+// void execute_commands(t_data *data)
+// {
+// 	t_command	*current;
+
+// 	current = data->command_top;
+// 	while (current != NULL)
+// 	{
+// 		// Rediriger les entrées/sorties si nécessaire
+// 		if (current->input_file != NULL)
+// 		{
+// 			data->fd_in = open(current->input_file, O_RDONLY);
+// 			dup2(data->fd_in, STDIN_FILENO);
+// 		}
+// 		if (current->output_file != NULL)
+// 		{
+// 			data->fd_out = open(current->output_file,
+//					O_WRONLY | O_CREAT | O_TRUNC, 0644);
+// 			dup2(data->fd_out, STDOUT_FILENO);
+// 		}
+
+// 		execute_command(current, data->env);
+
+// 		// Restaurer les descripteurs de fichiers standard
+// 		if (data->fd_in != STDIN_FILENO)
+// 		{
+// 			close(data->fd_in);
+// 			dup2(STDIN_FILENO, STDIN_FILENO);
+// 		}
+// 		if (data->fd_out != STDOUT_FILENO)
+// 		{
+// 			close(data->fd_out);
+// 			dup2(STDOUT_FILENO, STDOUT_FILENO);
+// 		}
+
+// 		current = current->next;
+// 	}
+// }
--- srcs/minishell.c
+++ srcs/minishell.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mbuchs <mbuchs@student.42.fr>              +#+  +:+       +#+        */
+/*   By: asuc <asuc@student.42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 17:08:03 by asuc              #+#    #+#             */
-/*   Updated: 2024/02/13 00:56:22 by mbuchs           ###   ########.fr       */
+/*   Updated: 2024/03/23 14:17:45 by asuc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int	g_return_code	= 0;
+int			g_return_code = 0;
 
 void	init_data(t_data *data)
 {
@@ -38,28 +38,33 @@ static int	ft_env(t_env *env)
 	return (0);
 }
 
-void	choose_case(t_env *env, t_data *data, char *line)
+void	choose_case(t_env *env, t_data *data)
 {
-	(void)line;
-	if (data->command_top == NULL || data->command_top->cmd == NULL)
-		return ;
-	if (ft_strcmp(data->command_top->cmd, "exit") == 0)
-		ft_exit(data, env, "exit", 0);
-	if (ft_strcmp(data->command_top->cmd, "cd") == 0)
-		ft_cd(data, env);
-	else if (ft_strcmp(data->command_top->cmd, "export") == 0)
-		ft_export(env, data->command_top->args[1]);
-	else if (ft_strcmp(data->command_top->cmd, "env") == 0)
-		ft_env(env);
-	else if (ft_strcmp(data->command_top->cmd, "unset") == 0)
-		ft_unset(env, data);
-	else if (ft_strcmp(data->command_top->cmd, "echo") == 0)
-		ft_echo(data);
-	else if (ft_strcmp(data->command_top->cmd, "pwd") == 0)
-		ft_pwd(env);
-	else
-		g_return_code = execve_path_env(data->command_top->cmd,
-			data->command_top->args, env, data);
+	t_command	*command;
+
+	command = data->command_top;
+	while (command)
+	{
+		if (data->command_top == NULL || data->command_top->cmd == NULL)
+			return ;
+		if (ft_strcmp(data->command_top->cmd, "exit") == 0)
+			ft_exit(data, env, "exit", 0);
+		if (ft_strcmp(data->command_top->cmd, "cd") == 0)
+			ft_cd(data, env);
+		else if (ft_strcmp(data->command_top->cmd, "export") == 0)
+			ft_export(env, data->command_top->args[1]);
+		else if (ft_strcmp(data->command_top->cmd, "env") == 0)
+			ft_env(env);
+		else if (ft_strcmp(data->command_top->cmd, "unset") == 0)
+			ft_unset(env, data);
+		else if (ft_strcmp(data->command_top->cmd, "echo") == 0)
+			ft_echo(data);
+		else if (ft_strcmp(data->command_top->cmd, "pwd") == 0)
+			ft_pwd(env);
+		else
+			g_return_code = execve_path_env(data->command_top->cmd,
+					data->command_top->args, env, data);
+	}
 }
 
 int	wait_cmd_prompt(t_data *data, t_env *env)
@@ -81,20 +86,18 @@ int	wait_cmd_prompt(t_data *data, t_env *env)
 		add_history(line);
 		lexer(line, data);
 		parser(data);
-		choose_case(env, data, line);
+		choose_case(env, data);
 		if (line != NULL)
 			free(line);
 		if (data->fd_out != 1)
 		{
-			// dup2(1, data->fd_out);
 			close(data->fd_out);
 			data->fd_out = 1;
 		}
 	}
 }
 
-int	main(int argc __attribute__((unused)),
-		char *argv[] __attribute__((unused)),
+int	main(int argc __attribute__((unused)), char *argv[] __attribute__((unused)),
 		char **envp)
 {
 	t_data	*data;
