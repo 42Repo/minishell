6555f435110840d673009c9006657942369fad2c
asuc <sucaurel@gmail.com>
2024-04-01 17:00:04 +0200

    update minishell and drawio and wakatime

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: asuc <asuc@student.42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 17:08:03 by asuc              #+#    #+#             */
-/*   Updated: 2024/03/23 15:48:19 by asuc             ###   ########.fr       */
+/*   Updated: 2024/03/29 12:43:36 by asuc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,9 +61,6 @@ void	execute_command(t_command *command, t_env *env, t_data *data,
 			dup2(output_fd, STDOUT_FILENO);
 			close(output_fd);
 		}
-		printf("command->cmd: %s\n", command->cmd);
-		for (int i = 0; command->args[i]; i++)
-			printf("command->args == %s\n", command->args[i]);
 		if (ft_strcmp(command->cmd, "exit") == 0)
 			ft_exit(data, env, "exit", 0);
 		else if (ft_strcmp(command->cmd, "cd") == 0)
@@ -111,11 +108,15 @@ void	choose_case(t_env *env, t_data *data)
 		}
 		execute_command(command, env, data, prev_fd, pipe_fd[1]);
 		if (prev_fd != STDIN_FILENO)
+		{
+			printf("close prev_fd\n");
 			close(prev_fd);
+		}
 		if (command->next)
 		{
 			close(pipe_fd[1]);
-			prev_fd = pipe_fd[0];
+			prev_fd = dup(pipe_fd[0]);
+			close(pipe_fd[0]);
 		}
 		command = command->next;
 	}
