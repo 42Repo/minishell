de7e2bc7d8dd45df15ff2bc264a3ffefa14a0854
mbuchs <mael@buchs.fr>
2024-05-18 23:11:45 +0200

    NON

--- srcs/lexer/expander/envar_2.c
+++ srcs/lexer/expander/envar_2.c
@@ -6,7 +6,7 @@
 /*   By: mbuchs <mbuchs@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 18:02:13 by mbuchs            #+#    #+#             */
-/*   Updated: 2024/05/18 23:03:55 by mbuchs           ###   ########.fr       */
+/*   Updated: 2024/05/18 23:10:00 by mbuchs           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -98,7 +98,10 @@ void	set_envar(t_data *data, t_token *selected, t_token *previous)
 	str = replace_envar(data, selected, 0, &tmp);
 	if (str && previous && previous->type == REDIR && (is_space_inside(str) || ft_strlen(str) == 0))
 	{
-		put_error("minishell:", str, ": ambiguous redirection\n");
+		if (ft_strlen(str) == 0)
+			put_error("minishell: ", selected->value, ": ambiguous redirection\n");
+		else
+			put_error("minishell: ", str, ": ambiguous redirection\n");
 		free(previous->value);
 		previous->value = NULL;
 		return ;
